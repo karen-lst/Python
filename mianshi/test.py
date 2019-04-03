3a24d50dff4ddb295d0ed11cb1a4427dae384609
karen-lst <15107334191@163.com>
2019-04-03 10:58:01 +0800

    Create test.py

---
+++ mianshi/test.py
@@ -0,0 +1,16 @@
+import os.path as op
+import os
+
+# # op.abspath('test.py')
+# print(op.abspath('test.py'))
+# print(op.normpath('D://Documents//GitHub//python-learning-notes//mianshi//test.py'))
+# # print(op.relpath('python3笔记.py'))
+# print(op.dirname('D://Documents//GitHub//python-learning-notes//mianshi//test.py'))
+# print(op.basename('D://Documents//GitHub//python-learning-notes//mianshi//test.py'))
+# print(op.exists('D://Documents//GitHub//python-learning-notes//mianshi//test.py'))
+
+# os.system("C:\\Windows\\System32\\calc.exe")
+
+os.getlogin()
+print(os.getlogin())
+print(os.cpu_count())
